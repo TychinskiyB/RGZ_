98dc681798dd7d06832ae1b262714055b6fe5690
Богдан Тычинский <tychinskiybogdan@gmail.com>
2023-12-22 17:24:58 +0700

    пробный

--- Db/models.py
+++ Db/models.py
@@ -1,23 +1,25 @@
 from . import db
 from flask_login import UserMixin
 
-#Таблица пользователи 
+
+
 class users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(102), nullable=False)
-    admin_on_off = db.Column(db.Boolean, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False)
 
-    def __repr__(self):
-        return f"id:{self.id}, username:{self.username}, admin_on_off:{self.admin_on_off}"
+    def repr(self):
+        return f"id:{self.id}, username:{self.username}, is_admin:{self.is_admin}"
 
-#Таблица книги
-class book (db.Model):
+
+class books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name_book = db.Column(db.String(100), nullable=False)
-    photo_link = db.Column(db.String, nullable=True)
-    avtor = db.Column(db.String, nullable=False)
-    izdatelstvo = db.Column(db.String, nullable=False)
-    ks= db.Column(db.Integer, nullable=False)
-    def __repr__(self):
-        return f"id:{self.id}, name_book:{self.name_book}, photo_link:{self.photo_link}, avtor:{self.avtor}, izdatelstvo:{self.izdatelstvo}, ks:{self.ks}"
+    book = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(1000), nullable=True)
+    author = db.Column(db.String(100), nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    publisher = db.Column(db.String(200), nullable=False)
+    
+    def repr(self):
+        return f"id:{self.id}, book:{self.book}, image_url:{self.image_url}, author:{self.author}, pages:{self.pages}, publisher:{self.publisher}"
--- app.py
+++ app.py
@@ -3,37 +3,89 @@ from flask import Flask, Blueprint, render_template, request, make_response, red
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from Db import db
-from Db.models import users, book
+from Db.models import users, books
 from flask_login import login_user, login_required, current_user, logout_user
 app = Flask(__name__)
 
 app.secret_key = "123"
-user_db = "Tychinskiy_rgz_ormm"
+user_db = "Tychinskiy_rgz_orm"
 host_ip = "localhost"
 host_port = "5432"
-database_name = "Tychinskiy_base_rgz_ormm"
+database_name = "Tychinskiy_base_rgz_orm"
 password = "123"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db.init_app(app)
-
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
 login_manager.init_app(app)
 
-@app.route('/')
-@app.route('/index')
-def start():
-    return redirect('/login', code=302)
-
 @login_manager.user_loader
 def load_users(user_id):
     return users.query.get(int(user_id))
 
-@app.route("/register", methods=['POST', 'GET'])
-def register():
+
+@app.route('/')
+@app.route('/index')
+def start():
+    return redirect('/mainpage', code=302)
+
+
+@app.route('/main', methods=['POST', 'GET'])
+def mainpage():
+    if current_user.is_authenticated:
+        username = current_user.username
+    else:
+        username = "Кто Вы?"
+    
+    if request.method == 'POST':
+        book_title = request.form.get('book')
+        author = request.form.get('author')
+        min_pages = request.form.get('min_pages')
+        max_pages = request.form.get('max_pages')
+        publisher = request.form.get('publisher')
+        
+        # Выполнить фильтрацию на основе введенных пользователем параметров
+        filtered_books_list = filter_books_logic(book_title, author, min_pages, max_pages, publisher)
+        return render_template('mainpage.html', all_books=filtered_books_list, username=username)
+
+    else:
+        # Раздел для отображения всех книг без фильтрации
+        offset = request.args.get('offset', default=0, type=int)
+        books_list = books.query.limit(20).offset(offset).all()
+        return render_template('mainpage.html', all_books=books_list, username=username)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error=''
+    if request.method=='GET':
+        return render_template('login.html')
+        
+    username_form=request.form.get('username')
+    password_form=request.form.get('password')
+
+    my_user=users.query.filter_by(username=username_form).first()
+    
+    if my_user=='' or password_form=='':
+        error='не заполнены поля'
+        return render_template('login.html', error=error)
+    
+    if my_user is not None:
+        if check_password_hash(my_user.password, password_form):
+            login_user(my_user, remember=False)
+            return redirect('/main')
+        else:
+            error='неверный пароль'
+            return render_template('login.html', error=error) 
+    else:
+        error='такого пользователя не существует'
+        return render_template('login.html', error=error) 
+
+    
+@app.route('/register', methods=['post', 'get'])
+def registerr():
     if request.method == 'GET':
         return render_template("register.html")
 
@@ -42,57 +94,23 @@ def register():
 
     if not username_form or not password_form:
         return render_template("register.html", errors='Заполните все поля')
-
+    
+    if not re.match("^[a-zA-Z0-9!@#$%^&*]+$", username_form) or not re.match("^[a-zA-Z0-9!@#$%^&*]+$", password_form):
+        return render_template("register.html", errors='Логин и пароль должны состоять только из латинских букв, цифр и знаков препинания')
+    
     if users.query.filter_by(username=username_form).first():
         return render_template("register.html", errors='Пользователь с таким логином уже существует')
 
     hashed_password = generate_password_hash(password_form, method="pbkdf2")
-    new_user = users(username=username_form, password=hashed_password, admin_on_off=False)
+    new_user = users(username=username_form, password=hashed_password, is_admin=False)
 
     db.session.add(new_user)
     db.session.commit()
     return redirect("/login")
 
-@app.route("/login", methods=['POST', 'GET'])
-def login():
-    if request.method == 'GET':
-        return render_template("login.html")
 
-    username_form = request.form.get("username")
-    password_form = request.form.get("password")
-
-    if not username_form or not password_form:
-        errors = 'Заполните все поля'
-        return render_template("login.html", errors=errors)
-
-    my_user = users.query.filter_by(username=username_form).first()
-
-    if my_user and (check_password_hash(my_user.password, password_form) or my_user.password == 'bogdan'):
-        login_user(my_user, remember=False)
-        return redirect("/book")
-    else:
-        errors = 'Неверное имя пользователя или пароль'
-        return render_template("login.html", errors=errors)
-    
-@app.route("/logout")
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect("/login")
-    
-@app.route("/", methods=['POST', 'GET'])
-@login_required
-def recipes_page():
-    username = (users.query.filter_by(id=current_user.id).first()).username
-    if request.method == 'GET':
-        all_recipes = recipes.query.all()
-        all_ingridients = []
-        for i in all_recipes:
-            all_ingridients.extend((i.ingridients.lower()).split(", "))
-        all_ingridients = list(set(all_ingridients))
-        print(all_ingridients)
-        return render_template('book.html', username=username, all_recipes=all_recipes)
-    else:
-        name = request.form.get('name')
-        all_recipes = recipes.query.filter(recipes.name_dish.ilike(f'%{name}%'))
-        return render_template('book.html', username=username, all_recipes=all_recipes)
+    return redirect('/login')
